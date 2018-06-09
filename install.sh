da27e8780f4f1c589881d0df131fa21bcd18da56
ldr <ldr@ldrdeMacBook-Air.local>
2018-06-09 15:16:29 +0800

    fix some bugs in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+CurrentDIR=`pwd`
 echo 'Cloning the Temperature driver'
 git clone https://github.com/lavoiesl/osx-cpu-temp.git && cd osx-cpu-temp
 echo 'make && make install'
 make && make install
 echo 'Driver install Successfully'
 
-cp -r ./CPUTemperature.app /Applications
+cd $CurrentDIR
+cp -r CPUTemperautre.app /Applications
 echo "FINISHED!!!!"
